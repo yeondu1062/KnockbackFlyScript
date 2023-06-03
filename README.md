da3c67824bf6223c088769825e5eb0d023fd82c0
연두 <108284815+yeondu1062@users.noreply.github.com>
2023-06-03 22:13:30 +0900

    추가

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# KnockbackFlyScript
-.
+//bridge-file-version: #0
+# 넉백 스크립트
+## 넉백으로 플레이어를 날려보냅니다. Minecraft Bedrock Gametest Script.
+
+2차 배포와 판매를 금지합니다.     
+오른쪽위에 초록색버튼 눌러서 zip로 다운받고 압축풀어서 행동팩 경로에 넣어주세요 :)
+
+### MADE BY YEONDU
+![icon](pack_icon.png)
---
+++ scripts/yeondu.js
@@ -0,0 +1,7 @@
+//bridge-file-version: #2
+import { system, world } from "@minecraft/server";
+system.events.beforeWatchdogTerminate.subscribe(evd => evd.cancel = true);
+
+const strength = [7, 1]; //날아가는 힘 [앞, 위]
+
+world.events.dataDrivenEntityTriggerEvent.subscribe(evd=>{if(evd.id == 'yeondu:nb') evd.entity.applyKnockback(evd.entity.getViewDirection().x, evd.entity.getViewDirection().z, strength[0], strength[1])});
